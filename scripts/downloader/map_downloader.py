2de1e3eee107af429812876be843f537f7d76c1f
aranwe <aranwes@gmail.com>
2012-08-24 11:06:03 +0200

    map_downloader.py: some edits in the download url creation

--- scripts/downloader/map_downloader.py
+++ scripts/downloader/map_downloader.py
@@ -16,39 +16,64 @@ def reporthook(a,b,c):
     sys.stdout.flush()
 
 def download_tile(server_id,map_type,x,y,z):
-     """
-      ***server_id: name (map_type: desc)
-      1: maps.google.com (1: satellite, 2: normal, 3: terrain)
-      2: mapy.cz (1: normal, 2: turistic, 3: aircraft)
-      3: openstreetmap.org (1: standard, 2: cyclo, 3: transport, 4: mapquest)
-     
-      x = horizontal id
-      y = vertical id
-      z = zoom
-      ***URL_patterns
-      ***googlemaps
-      1:1 src = khms0.google.com/kh/v=115&src=app&x=1&y=6&z=4 + http://mts0.google.com/vt/src=app&x=3&y=5&z=4&hl=en&lyrs=h // first jpeg , second png
-      1:2 src = http://mts0.google.com/vt/src=app&x=3&y=5&z=4&hl=en&lyrs=m // png
-      1:3 src = http://mts0.google.com/vt/src=app&x=21&y=11&z=5&hl=cs&lyrs=t,r // jpeg
-      ****mapy.cz
-      2:1 src = m1.mapserver.mapy.cz/base-n/8_8000000_7f00000 -  posledni cisla z_x_y //gif
-      2:2 src = m1.mapserver.mapy.cz/turist/8_8000000_7f00000 -  posledni cisla z_x_y // png
-      2:3 src = m1.mapserver.mapy.cz/ophoto/8_8000000_7f00000 + m0.mapserver.mapy.cz/hybrid/8_8000000_7f00000 -  posledni cisla z_x_y // jpeg
-      ***openstreetmaps.org
-      3:1 src = a.tile.openstreetmap.org/9/286/173.png - posledni tri cisla: z/x/y.png
-      3:2 src = a.tile.opencyclemap.org/cycle/9/286/173.png - posledni tri cisla: z/x/y.png
-      3:3 src = a.tile2.opencyclemap.org/transport/9/286/173.png - posledni tri cisla z/x/y.png
-      3:4 src = otile1.mqcdn.com/tiles/1.0.0/osm/9/291/174.png - posledni tri cisla: z/x/y.png
-     """
-    filename=folder+'/tile_'+x+'_'+y+'_'+z+'.'+ext
-    i = url.rfind('/')
-    file = url[i+1:]
-    print url, "->", file
-    urllib.urlretrieve(url, file, reporthook)
-    
-    
-    
-    
+          """
+              ***server_id: name (map_type: desc)
+              0: maps.google.com (1: satellite, 2: normal, 3: terrain)
+              1: mapy.cz (1: normal, 2: turistic, 3: aircraft)
+              2: openstreetmap.org (1: standard, 2: cyclo, 3: transport, 4: mapquest)
+             
+              x = horizontal id
+              y = vertical id
+              z = zoom
+              ***URL_patterns
+              ***googlemaps
+              0:0 src = khms0.google.com/kh/v=115&src=app&x=1&y=6&z=4 + http://mts0.google.com/vt/src=app&x=3&y=5&z=4&hl=en&lyrs=h // first jpeg , second png
+              0:1 src = http://mts0.google.com/vt/src=app&x=3&y=5&z=4&hl=en&lyrs=m // png
+              0:2 src = http://mts0.google.com/vt/src=app&x=21&y=11&z=5&hl=cs&lyrs=t,r // jpeg
+              ****mapy.cz
+              1:0 src = m1.mapserver.mapy.cz/base-n/8_8000000_7f00000 -  posledni cisla z_x_y //gif
+              1:1 src = m1.mapserver.mapy.cz/turist/8_8000000_7f00000 -  posledni cisla z_x_y // png
+              1:2 src = m1.mapserver.mapy.cz/ophoto/8_8000000_7f00000 + m1.mapserver.mapy.cz/hybrid/8_8000000_7f00000 -  posledni cisla z_x_y // jpeg, second png
+              ***openstreetmaps.org
+              2:0 src = a.tile.openstreetmap.org/9/286/173.png - posledni tri cisla: z/x/y.png
+              2:1 src = a.tile.opencyclemap.org/cycle/9/286/173.png - posledni tri cisla: z/x/y.png
+              2:2 src = a.tile2.opencyclemap.org/transport/9/286/173.png - posledni tri cisla z/x/y.png
+              2:3 src = otile1.mqcdn.com/tiles/1.0.0/osm/9/291/174.png - posledni tri cisla: z/x/y.png
+          """
+          url_struct = [
+          [
+          ['khms0.google.com/kh/v=115&src=app&x=%x&y=%y&z=%z', 'jpg', 'http://mts0.google.com/vt/src=app&x=%x&y=%y&z=%z&hl=en&lyrs=h', 'png'],
+          ['http://mts0.google.com/vt/src=app&x=%x&y=%y&z=%z&hl=en&lyrs=m', 'png'],
+          ['http://mts0.google.com/vt/src=app&x=%x&y=%y&z=%z&hl=cs&lyrs=t,r', 'jpg']
+          ],
+          [
+          ['http://m1.mapserver.mapy.cz/base-n/%z_%x_%y', 'gif'],
+          ['http://m1.mapserver.mapy.cz/turist/%z_%x_%y', 'png'],
+          ['http://m1.mapserver.mapy.cz/ophoto/%z_%x_%y', 'jpg', 'http://m1.mapserver.mapy.cz/hybrid/%z_%x_%y', 'png']
+          ],
+          [
+          ['http://a.tile.openstreetmap.org/%z/%x/%y.png', 'png'],
+          ['http://a.tile.opencyclemap.org/cycle/%z/%x/%y.png', 'png'],
+          ['http://a.tile2.opencyclemap.org/transport/%z/%x/%y.png', 'png'],
+          ['http://otile1.mqcdn.com/tiles/1.0.0/osm/%z/%x/%y.png', 'png']
+          ]
+          ]
+          edits = [('%x',x),('%y',y),('%z',z)]
+          url1=url_struct[server_id][map_type][1]
+          for search, replace in edits:
+                    url1 = url1.replace(search, replace)
+
+          if len(url_struct[server_id][map_type])>2:
+                url2=url_struct[server_id][map_type][3]
+                for search, replace in edits:
+                        url1 = url1.replace(search, replace)
+                
+          filename=folder+'/tile_'+x+'_'+y+'_'+z+'.'+ext
+          i = url.rfind('/')
+          file = url[i+1:]
+          print url, "->", file
+          urllib.urlretrieve(url, file, reporthook)
+
 """
  my notes:
  
